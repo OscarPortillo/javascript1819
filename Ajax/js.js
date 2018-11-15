031d440d0f448578cc7a4a05200b24e8b2b30257
Oscar Portillo <oscar94.oscar@hotmail.com>
2018-11-15 18:54:44 +0100

    Ajax y Json mostrando datos de la imagen

--- Ajax/js.js
+++ Ajax/js.js
@@ -12,16 +12,26 @@ function cargarAjax() {
     function mostrar() {
         console.log("Estamnos en mostrar");
         console.log(http.readyState);
-		console.log(http.status);
-		
+        console.log(http.status);
+
         if (http.readyState == 4 && http.status == 200) {
             console.log("Recibimos datos de ajax");
-			console.log(http.responseText);
-			var obj =JSON.parse(this.responseText);
-			document.getElementById("cupcake").innerHTML = obj.cupcakes[0].ID
-			+obj.cupcakes[0].nombre;
+            console.log(http.responseText);
+            var obj = JSON.parse(this.responseText);
+            for (let i = 0; i < 5 ; i++) {
+                let ruta = obj.cupcakes[i].ruta;
+               document.getElementById("cupcake"+""+i).innerHTML += obj.cupcakes[i].ID +
+                    "<br>" + "<img src='" + ruta + "'/><br>"
+                +obj.cupcakes[i].nombre+"<br>"
+                +obj.cupcakes[i].precio+"<br>";
+                for(let j = 0 ; j < obj.cupcakes[i].categorias.length; j++){
+                    document.getElementById("cupcake"+""+i).innerHTML+= obj.cupcakes[i].categorias[j]+"<br> ";
+                }
+                 document.getElementById("cupcake"+""+i).innerHTML+= obj.cupcakes[i].descripcion;
+            }
+
         }
         console.log("Salimos de mostrar");
-		
+
     }
 }
