db4262ca310dc49872930adf43085d2d80687f12
Oscar Portillo <oscar94.oscar@hotmail.com>
2018-11-15 17:52:16 +0100

    Ajax y Json mostrando datos de la imagen

--- Ajax/js.js
+++ Ajax/js.js
@@ -7,16 +7,21 @@ function cargarAjax() {
     let http = new XMLHttpRequest();
     http.onreadystatechange = mostrar;
     http.open("GET", "./cupcakes.json", true);
-    http.send(null);
+    http.send();
 
     function mostrar() {
         console.log("Estamnos en mostrar");
         console.log(http.readyState);
+		console.log(http.status);
+		
         if (http.readyState == 4 && http.status == 200) {
             console.log("Recibimos datos de ajax");
+			console.log(http.responseText);
+			var obj =JSON.parse(this.responseText);
+			document.getElementById("cupcake").innerHTML = obj.cupcakes[0].ID
+			+obj.cupcakes[0].nombre;
         }
         console.log("Salimos de mostrar");
+		
     }
 }
-
-
