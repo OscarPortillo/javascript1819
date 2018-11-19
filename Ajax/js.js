2142cd7000da0c9ada7a053b0c4149a7f043b7f7
Oscar Portillo <oscar94.oscar@hotmail.com>
2018-11-19 16:39:59 +0100

    Ajax y Json ver datos desde otra funcion en  proceso

--- Ajax/js.js
+++ Ajax/js.js
@@ -5,33 +5,47 @@ window.onload = function () {
 function cargarAjax() {
     // alert("Arranque");
     let http = new XMLHttpRequest();
-    http.onreadystatechange = mostrar;
+    http.onreadystatechange =  mostrar;
     http.open("GET", "./cupcakes.json", true);
     http.send();
 
     function mostrar() {
-        console.log("Estamnos en mostrar");
-        console.log(http.readyState);
-        console.log(http.status);
+        //console.log("Estamnos en mostrar");
+        //console.log(http.readyState);
+        //console.log(http.status);
 
         if (http.readyState == 4 && http.status == 200) {
-            console.log("Recibimos datos de ajax");
-            console.log(http.responseText);
+            //console.log("Recibimos datos de ajax");
+            //console.log(http.responseText);
             var obj = JSON.parse(this.responseText);
-            for (let i = 0; i < 5 ; i++) {
+            for (let i = 0; i < obj.cupcakes.length ; i++) {
                 let ruta = obj.cupcakes[i].ruta;
-               document.getElementById("cupcake"+""+i).innerHTML += obj.cupcakes[i].ID +
-                    "<br>" + "<img src='" + ruta + "'/><br>"
-                +obj.cupcakes[i].nombre+"<br>"
+                var img = document.createElement("img");
+                img.src=ruta;
+                var div = document.getElementById("cupcake"+i);
+                div.innerHTML += obj.cupcakes[i].ID+"<br>";
+                div.append(img);
+                div.innerHTML+=obj.cupcakes[i].nombre+"<br>"
                 +obj.cupcakes[i].precio+"<br>";
-                for(let j = 0 ; j < obj.cupcakes[i].categorias.length; j++){
-                    document.getElementById("cupcake"+""+i).innerHTML+= obj.cupcakes[i].categorias[j]+"<br> ";
+                for(let j = 0 ; j < obj.cupcakes[i].categorias.length; j++){/*para recorrer las etiquetas*/
+                    var lista = document.createElement("ul");
+                    var li =  document.createElement("li")
+                    lista.append(li);
+                    div.append(lista);
+                    li.innerHTML+= obj.cupcakes[i].categorias[j];
                 }
-                 document.getElementById("cupcake"+""+i).innerHTML+= obj.cupcakes[i].descripcion;
+                 div.innerHTML+= obj.cupcakes[i].descripcion;
+                
             }
 
         }
         console.log("Salimos de mostrar");
-
     }
+    
+    
+    
+}
+function mostrarDatos(objeto)
+{
+    //meter aqui las cosas para ver los datos desde aqui
 }
