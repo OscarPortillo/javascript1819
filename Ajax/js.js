6698b78995ae87988ae631e8c1c198cadc77d225
Oscar Portillo <oscar94.oscar@hotmail.com>
2018-12-03 16:43:26 +0100

    ALV COMPA

--- Ajax/js.js
+++ Ajax/js.js
@@ -1,9 +1,15 @@
+var objetoCupcakes;
+
 window.onload = function () {
     cargarAjax();
 }
+
+
+
 Array.prototype.unique = function () {
     return this.filter((x, i, a) => a.indexOf(x, i + 1) < 0);
 }
+
 function cargarAjax() {
     let http = new XMLHttpRequest();
     http.onreadystatechange = mostrar;
@@ -12,10 +18,12 @@ function cargarAjax() {
 
     function mostrar() {
         if (http.readyState == 4 && http.status == 200) {
-            var obj = JSON.parse(this.responseText);
-            verDatos(obj);
-            verCategorias(obj);
-            mostrarCategoria(obj);
+            let datosCupcke = http.responseText;
+            objetoCupcakes = JSON.parse(datosCupcke);
+            verDatos(objetoCupcakes);
+            verCategorias(objetoCupcakes);
+            mostrarCategoria(objetoCupcakes);
+            ordenarPrecioDescendente(objetoCupcakes);
         }
         console.log("Salimos de mostrar");
     }
@@ -50,26 +58,55 @@ function verDatos(objeto) {
 
 function verCategorias(objeto) {
     for (let i = 0; i < objeto.cupcakes.length; i++) {
-        for (let j = 0; j < objeto.cupcakes[i].categorias.length; j++) {
+        for (let j = 0; j < objeto.cupcakes[i].etiquetas.length; j++) {
             let eti = document.getElementById("etiquetas");
-            var etiqueta = [objeto.cupcakes[i].categorias[j]];
+            var etiqueta = [objeto.cupcakes[i].etiquetas[j]];
             eti.innerHTML += etiqueta + "<br>";
         }
     }
-}//ver categorías
-function mostrarCategoria(objeto){
-    var arraYCategorias= [];
+} //ver categorías
+function mostrarCategoria(objeto) {
+    var arraYCategorias = [];
     var arraYCategoriasFiltrado = [];
     for (let i = 0; i < objeto.cupcakes.length; i++) {
         for (let j = 0; j < objeto.cupcakes[i].categorias.length; j++) {
             arraYCategorias.push(objeto.cupcakes[i].categorias[j]);
             arraYCategoriasFiltrado = arraYCategorias.unique();
         }
-    }    
-    for ( let categoria of arraYCategoriasFiltrado){
+    }
+    for (let categoria of arraYCategoriasFiltrado) {
         let select = document.getElementById("mostrarCategoria");
         let option = document.createElement("option");
         select.append(option);
         option.innerHTML += categoria;
     }
+} //mostrarCategoria
+
+function descendent() {
+    console.log("Hola mundo");
+    //ordenarPrecioDescendente(objetoCupcakes);
+    
+    OrdenarPorIdAscendente(objetoCupcakes);
 }
+
+function ordenarPrecioDescendente(objeto) {
+    var arrayPrecios;
+    for (let i = 0; i < objeto.cupcakes.length; i++) {
+        //console.log(objeto.cupcakes[i].precio);
+        arrayPrecios = objeto.cupcakes[i].precio;
+    }
+}
+function OrdenarPorIdAscendente(objetoCupcakes) {
+    for(let cupcake of objetoCupcakes.cupcakes){
+        return ( (cupcake.precio == cupcake.precio)? 0 : (cupcake.precio > cupcake.precio));
+    }
+	
+
+}
+/*apuntes clase
+ejemploc.addEventListener("click",function(){
+    vender(parametro){
+        
+    }
+},true);
+*/
