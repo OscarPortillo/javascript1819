ef2368e5968c8f2e4e7cee2801dd4b494c052ff9
Oscar Portillo <oscar94.oscar@hotmail.com>
2018-12-10 20:02:15 +0100

    examen

--- examen1801/ejercicio5.js
+++ examen1801/ejercicio5.js
@@ -17,7 +17,8 @@ function verDatos() {
             let r = http.responseText;
             objeto = JSON.parse(r);
             mostrarDatos();
-            mostraMAcroarea();
+            mostrarMacroarea();
+            filtrarMacroAreas();
         }
     }
 } //verDatos
@@ -52,7 +53,7 @@ function mostrarDatos() {
     }
 }
 
-function mostraMAcroarea() {
+function mostrarMacroarea() {
     var opc = document.getElementById("opciones");
     var array = [];
     var filtrado = [];
@@ -68,19 +69,45 @@ function mostraMAcroarea() {
     }
 }
 
-function verMacroareasFiltradas() {
-    var arrayMAcro = [];
-    var arrayMacroFilt = [];
-    for (area of objeto.datos) {
-        for (area of competiciones.competiciones) {
-            arrayMAcro.push(area);
-            arrayMacroFilt = arrayMAcro.unique();
-        }
-    }
+function filtrarMacroAreas() {
     var select = document.getElementById("opciones");
-    for (macro of arrayMacroFilt) {
-        var option = document.createElement("option");
-        select.append(option);
-        option.innerHTML = macro;
+    select.addEventListener('change',
+        function () {
+            var competicion = this.options[select.selectedIndex];
+            console.log(competicion.value)
+            var macroAreaFiltrada = objeto.datos.filter(a => a.MACROAREA.includes(competicion.value));
+            document.getElementById("respuesta").innerHTML = '';
+        console.log(macroAreaFiltrada);
+            verMacroAreasFiltrada(macroAreaFiltrada);
+        });
+}
+
+function verMacroAreasFiltrada(macroAreaFiltrada) {
+    var div = document.getElementById("respuesta");
+    objeto.datos.sort(function (a, b) {
+        return a.MACROAREA.localeCompare(b.MACROAREA);
+    });
+
+    for (dato of macroAreaFiltrada) {
+        var tr = document.createElement("tr");
+        var td1 = document.createElement("td");
+        var td2 = document.createElement("td");
+        var td3 = document.createElement("td");
+        var td4 = document.createElement("td");
+        var td5 = document.createElement("td");
+        var td6 = document.createElement("td");
+        tr.append(td1);
+        tr.append(td2);
+        tr.append(td3);
+        tr.append(td4);
+        tr.append(td5);
+        tr.append(td6);
+        td1.innerHTML = dato.MACROAREA;
+        td2.innerHTML = dato.ANO;
+        td3.innerHTML = dato.EDAD;
+        td4.innerHTML = dato.NUMERO_PDI;
+        td5.innerHTML = dato.CATEGORIA_CUERPO_ESCALA;
+        td6.innerHTML = dato.SEXO;
+        div.append(tr);
     }
 }
