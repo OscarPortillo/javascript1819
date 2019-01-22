d082573fecd03bfb2bf9b19e90a4905c42ab3c47
Oscar Portillo <oscar94.oscar@hotmail.com>
2019-01-22 16:41:08 +0100

    HACER EN CASA LA TAREA DE CUADROS JQUERY

--- jQuery/Ejercicios/tareaCuadros.js
+++ jQuery/Ejercicios/tareaCuadros.js
@@ -1,31 +1,12 @@
 $(document).ready(function () {
     $(".cuadrante").hover(function () {
-        funcion1();
+        desaparecer(1,4);
         
     })
 });
-var funcion1 =function(){
-    $("#c1").hide('slow',funcion2);
+
+var desaparecer = function(a,b){
+    $("#c"+a).hide("slow");
 }
-var funcion2 =function(){
-    $("#c2").hide('slow',funcion3);
-}
-var funcion3 =function(){
-    $("#c3").hide('slow',funcion4);
-}
-var funcion4 =function(){
-    $("#c4").hide('slow');
-    funcionShow1();
-}
-var funcionShow1 =function(){
-    $("#c1").show('slow',funcionShow2);
-}
-var funcionShow2 =function(){
-    $("#c2").show('slow',funcionShow3);
-}
-var funcionShow3 =function(){
-    $("#c3").show('slow',funcionShow4);
-}
-var funcionShow4 =function(){
-    $("#c4").show('slow');
-}
+
+//sumas 1 a la a y haces un callback con la misma función que es desparecer (a, b) y la a pasará a ser 2 antes tendrás que combrobar si a<b
