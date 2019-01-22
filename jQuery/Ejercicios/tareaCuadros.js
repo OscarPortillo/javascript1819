3f81ff156fb4e7c52cfdeb7ca6541cafcd979110
Oscar Portillo <oscar94.oscar@hotmail.com>
2019-01-22 18:40:00 +0100

    tarea terminada

--- jQuery/Ejercicios/tareaCuadros.js
+++ jQuery/Ejercicios/tareaCuadros.js
@@ -1,12 +1,25 @@
 $(document).ready(function () {
-    $(".cuadrante").hover(function () {
-        desaparecer(1,4);
-        
+    $("#contenedor").one("mouseover", function () {
+        desaparecer(1);
     })
 });
-
-var desaparecer = function(a,b){
-    $("#c"+a).hide("slow");
+var desaparecer = function (a) {
+    $("#c" + a).hide("slow", function () {
+        a = a + 1;
+        $("#c" + a).hide("slow", desaparecer(a));
+    });
+    if (a == 4) {
+        mostrar(1);
+    }
+}
+var mostrar = function (b) {
+    $("#c" + b).show("fast", function () {
+        b = b + 1;
+        $("#c" + b).show("fast", mostrar(b));
+    });
 }
 
 //sumas 1 a la a y haces un callback con la misma función que es desparecer (a, b) y la a pasará a ser 2 antes tendrás que combrobar si a<b
+
+
+///mirar el off
