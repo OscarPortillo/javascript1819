8899f57a9e5a71098b9dd273476c5c86817b3731
OscarPortillo <oscar_94.oscar@hotmail.com>
2019-01-24 11:50:39 +0100

    casa

--- jQuery/Ejercicios/tareaCuadros.js
+++ jQuery/Ejercicios/tareaCuadros.js
@@ -1,23 +1,17 @@
 $(document).ready(function () {
     $("#contenedor").one("mouseover", function () {
-        desaparecer(1);
-    })
+        for (var i = 1; i <= 4; i++) {
+            $("#c" + i).animate({
+                width: '0px',
+                height: '0px'
+            }, 2005).delay(1000).animate({
+                width: '+100%',
+                height: '+100%'
+            }, 2000).delay(1000);
+        }
+    });
 });
-var desaparecer = function (a) {
-    $("#c" + a).hide("slow", function () {
-        a = a + 1;
-        $("#c" + a).hide("slow", desaparecer(a));
-    });
-    if (a == 4) {
-        mostrar(1);
-    }
-}
-var mostrar = function (b) {
-    $("#c" + b).show("fast", function () {
-        b = b + 1;
-        $("#c" + b).show("fast", mostrar(b));
-    });
-}
+
 
 //sumas 1 a la a y haces un callback con la misma función que es desparecer (a, b) y la a pasará a ser 2 antes tendrás que combrobar si a<b
 
