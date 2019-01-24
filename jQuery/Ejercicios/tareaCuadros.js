7997036f41b2bf78aa475ab20180cff832c3827b
Oscar Portillo <oscar94.oscar@hotmail.com>
2019-01-24 16:50:42 +0100

    cuadros terminado

--- jQuery/Ejercicios/tareaCuadros.js
+++ jQuery/Ejercicios/tareaCuadros.js
@@ -1,19 +1,35 @@
+var contador = 0;
 $(document).ready(function () {
     $("#contenedor").one("mouseover", function () {
-        for (var i = 1; i <= 4; i++) {
-            $("#c" + i).animate({
-                width: '0px',
-                height: '0px'
-            }, 2005).delay(1000).animate({
-                width: '+100%',
-                height: '+100%'
-            }, 2000).delay(1000);
-        }
+        ocultar(1);
     });
+    contador= $('.contenido').length;
 });
+function ocultar(i) {    
+    if (i <= contador) {
+        $('#c' + i).hide('slow', function () {
+            i++;
+            ocultar(i);
+        });
+    } else {
+        i = 1;
+        mostrar(i);
+    }
+}
+function mostrar(i) {
+    if (i <= contador) {
+        $('#c' + i).show('fast', function () {
+            i++;
+            mostrar(i);
+        });
+    }
+}
 
 
 //sumas 1 a la a y haces un callback con la misma función que es desparecer (a, b) y la a pasará a ser 2 antes tendrás que combrobar si a<b
 
 
-///mirar el off
+/*mirar el off.animate({
+                width: '+100%',
+                height: '+100%'
+            }, 2000).delay(1000);*/
