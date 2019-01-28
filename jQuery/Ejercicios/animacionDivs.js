f15b98c7b4b2529dc28693b6890b98f690462fe8
Oscar Portillo <oscar94.oscar@hotmail.com>
2019-01-28 16:41:14 +0100

    animar divs (Ondas)

---
+++ jQuery/Ejercicios/animacionDivs.js
@@ -0,0 +1,60 @@
+$(document).ready(function () {
+
+    mostrarDivs();
+    AñadirEfecto();
+});
+
+function mostrarDivs() {
+    for (var i = 1; i < 30; i++) {
+        $('.contenedor').append('<div class="tubo"></div>');
+    }
+}
+
+function AñadirEfecto() {
+    $(".tubo").hover(
+        function () {
+            $(this).stop().animate({
+            height: '600px'
+        }, 'fast', 'linear');
+        },
+        function () {
+            $(this).stop().animate({
+            height: '300px'
+        }, 'fast', 'linear');
+        }
+    );
+}
+
+
+
+/*$(document).ready(function () {
+
+    mostrarDivs();
+    estirar();
+    encoger();
+});
+
+function mostrarDivs() {
+    for (var i = 1; i < 30; i++) {
+        $('.contenedor').append('<div class="tubo"></div>');
+    }
+}
+
+function estirar() {
+    $('.tubo').mouseover(
+        function () {
+        $(this).stop().animate({
+            height: '600px'
+        }, 'fast', 'linear');
+    });
+
+}
+
+function encoger() {
+    $('.tubo').mouseout(function () {
+    $(this).stop().animate({
+        height: '300px'
+    }, 'fast', 'linear');
+    });
+}
+*/
