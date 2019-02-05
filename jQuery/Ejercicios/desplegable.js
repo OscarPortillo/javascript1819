76530c72247fd38ea759942b951878158030ad08
Oscar Portillo <oscar94.oscar@hotmail.com>
2019-02-05 18:29:19 +0100

    Tarea menu

--- jQuery/Ejercicios/desplegable.js
+++ jQuery/Ejercicios/desplegable.js
@@ -1,31 +1,31 @@
 $(function () {
-    console.log("entrada");
+    //console.log("entrada");
     cargarAjax();
-    console.log("Final");
+   // console.log("Final");
 });
 
 
 
-var cargarAjax = function(){
+var cargarAjax = function () {
     $.ajax({
         url: "./desplegable.json",
-        dataType : "json"
-    }).done(function(respuesta){
-        console.log("Lectura ajax");
-        console.log(respuesta);
+        dataType: "json"
+    }).done(function (respuesta) {
+        //console.log("Lectura ajax");
+        //console.log(respuesta);
         pintarDatos(respuesta);
-        console.log("final Lectura ajax");
-    }).fail(function(){
-       console.log("Fallo") ;
-    }).always(function(){
-        document.write("<p>Finalizando</p>");
+       // console.log("final Lectura ajax");
+    }).fail(function () {
+       // console.log("Fallo");
+    }).always(function () {
+        //        document.write("<p>Finalizando</p>");
     });
 }
-
-var pintarDatos = function(respuesta){
-    
-    for(m of respuesta.menu){
-        document.write(m.denominacion+"<br>");
-        document.write(m.url+"<br>");
+var pintarDatos = function (respuesta) {
+    var menu = document.getElementById("menu");
+    var ul = document.createElement("ul");
+    for (m of respuesta.menu) {
+        $("ul").append("<li>"+m.denominacion+"<br><a href='#'>"+m.url+"</a><button class='boton'>&rarr;</button></li>");
+        menu.append(ul);
     }
 }
