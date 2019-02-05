03aa2c5d4e0c4100b327ad561dc30b7c97dce823
Oscar Portillo <oscar94.oscar@hotmail.com>
2019-02-05 16:38:08 +0100

    Tarea menu

--- jQuery/Ejercicios/desplegable.js
+++ jQuery/Ejercicios/desplegable.js
@@ -1,55 +1,31 @@
-var objeto;
-$(document).ready(function () {
-    var cadena = $.getJSON("desplegable.json", function (dato) {
-        objeto = dato;
-        verDatos();
-        mostrarSubMenu();
-    })
+$(function () {
+    console.log("entrada");
+    cargarAjax();
+    console.log("Final");
 });
 
-function verDatos() {
-    console.log(objeto)
-    var lista = $("#menu");
-    var ul = document.createElement("ul");
-    for (dato of objeto.menu) {
-        var li = document.createElement("li");
-        var flecha = document.createElement("button");
-        li.innerHTML = dato.denominacion;
-        $(li).addClass("has-sub");
-        li.append(flecha);
-        $(flecha).addClass("boton");
-        flecha.innerHTML = "&rarr;";
-        var ul2 = document.createElement("ul");
-        if (dato.hijos != null) {
 
-            for (hijo1 of dato.hijos) {
-                var li2 = document.createElement("li");
-                $(li2).addClass("has-sub");
-                li2.innerHTML = hijo1.denominacion;
-                li.append(ul2);
-                ul2.append(li2);
-            }
-        }
-        
-        ul.append(li);
-        lista.append(ul);
+
+var cargarAjax = function(){
+    $.ajax({
+        url: "./desplegable.json",
+        dataType : "json"
+    }).done(function(respuesta){
+        console.log("Lectura ajax");
+        console.log(respuesta);
+        pintarDatos(respuesta);
+        console.log("final Lectura ajax");
+    }).fail(function(){
+       console.log("Fallo") ;
+    }).always(function(){
+        document.write("<p>Finalizando</p>");
+    });
+}
+
+var pintarDatos = function(respuesta){
+    
+    for(m of respuesta.menu){
+        document.write(m.denominacion+"<br>");
+        document.write(m.url+"<br>");
     }
 }
-
-function mostrarSubMenu() {
-    $(".boton").click(
-        function(){
-            $(" #menu ul li > ul").css("visibility"," visible" );
-        }
-    );
-   
-}
-
-
-
-
-
-
-
-
-
