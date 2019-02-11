32b5fe0d1c6be90c81a9ef80e2eb2721ae114e34
OscarPortillo <oscar_94.oscar@hotmail.com>
2019-02-11 14:32:33 +0100

    tarea lista

--- jQuery/Ejercicios/ivan/ivan.js
+++ jQuery/Ejercicios/ivan/ivan.js
@@ -1,5 +1,5 @@
-$(document).ready(function () {
-    $(function () { // si paso como parámetro en function() "event" luego hago let w = $(this).attr("href") cogeré el atributo de ese enlace que hemos pasado por "event" ||| .after .before es para meterle html a un elemento (event.after("falló") como el append)
+$(document).ready(function() {
+    $(function() {
         cargarAjax("https://codepen.io/IvanArasco/pen/xMrMPe.js", false);
     })
 })
@@ -13,58 +13,50 @@ function cargarAjax(web, cacheada) {
         .fail(cargaNoOk);
 }
 
-$("#btn").on("click", function () { //desplegar / ocultar menú
-    $("#menu").slideToggle(1000)
-});
+
 
 let cad = "";
 
 function pintar(respuesta) {
-    var id = 1;
-    var nivel = "nivel";
     //console.log(respuesta)
-    for (m of respuesta) {
-        cad += "<ul>"
-        cad += "<li id="+nivel+id+"> <strong>" + m.denominacion + "</strong> <br>"
-        if (m.hijos) {
-            cad += "<input type='button' class='flecha' value='&#8595'>"
+    var nivel = "nivel";
+    var conta = 1;
+    cad = "<ul>";
+    for(m of respuesta){
+        cad += "<li id="+nivel+""+conta+">"+m.denominacion+"</li>";
+        if(m.hijos){
+//            /console.log("tiene hijos")
+            pintarHijos(m.hijos,(nivel+""+conta));
         } else {
-            cad += '<br>'
+            
+            //console.log("no tiene hijos")
+            
         }
-        if (m.target) {
-            cad += '<a href=' + m.url + ' target=' + m.target + '> www.google.es </a>'
-        } else {
-            cad += ' <a href=' + m.url + '> www.google.es </a></li> '
-        }
-        cad += '<br>'
-        if (m.hijos) {
-            console.log(m)
-            console.log("Tiene hijos")
-            pintar(m.hijos)
-        }
-        cad += "</ul>";
-        id++;
+        conta++;
     }
-
+    cad +="</ul>";
+}
+function pintarHijos(respuesta,nivel){
+    console.log(nivel)
+    conta = 1;
+    var cadHijo="";
+    console.log("Estamos con los hijos")
+    $("#"+nivel).append(cadHijo)
+    cadHijo = "<ul>";
+    for(m of respuesta){
+        cadHijo +="<li id="+nivel+""+conta+">"+m.denominacion+"</li>";
+        console.log(m)
+    }
+    cadHijo+="</ul>";
 }
 
-function cargaOk(respuesta) {
+function cargaOk(respuesta) { //cuando lee el json correctamente
     pintar(respuesta.menu)
-    $("#menu").append(cad)
-
-    $(".flecha").on("click", function () {
-        let hijos = $(this).parent().parent().children() // el elemento seleccionado está bajo un li y bajo un ul
-        if (hijos.is(':visible')) {
-            console.log(hijos)
-            hijos.hide();
-            $(".flecha").show();
-        } else {
-            hijos.show();
-        }
-    })
+    $("#menu").append(cad) // todo el contenido lo metemos en el html
+    
 
 }
 
-function cargaNoOk(objeto_jqXHR, estatus, errorDevuelto) {
+function cargaNoOk(objeto_jqXHR, estatus, errorDevuelto) { // cuando lee mal el json
     var r = "ESTATUS ES: " + estatus + "<br>Error devuelto: " + errorDevuelto;
 }
