589b5d3ca5d6c105a306b6e0ed392e63d463af35
oscarportillo <oscar94.oscar@hotmail.com>
2019-04-20 16:57:15 +0200

    hacer aplicacion para practicar ingles

---
+++ ingles Java Script/main.js
@@ -0,0 +1,30 @@
+window.onload = function () {
+    verDatos();
+}
+var objeto;
+Array.prototype.unique = function () {
+    return this.filter((x, i, a) => a.indexOf(x, i + 1) < 0);
+}
+
+function verDatos() {
+    http = new XMLHttpRequest();
+    http.onreadystatechange = mostrar;
+    http.open("GET", "./verbs.json", true);
+    http.send(null);
+
+    function mostrar() {
+        if (http.readyState == 4 && http.status == 200) {
+            let r = http.responseText;
+            objeto = JSON.parse(r);
+            crearTabla();
+        }
+    }
+} //verDatos
+function crearTabla() {
+    var datos = document.getElementById("datos");
+    for (verbo in objeto) {
+        for (verb of objeto[verbo]) {
+            console.log(verb)
+        }
+    }
+}
